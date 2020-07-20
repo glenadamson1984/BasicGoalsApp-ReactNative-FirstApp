7f15f5828093ef31e1b06ee4fd3e717b52687f6a
Glen Adamson <glen.adamson@relative.uk.com>
2020-07-20 11:00:57 +0100

    updated app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
+  const [outputText, setText] = useState('Open up App.tsx to start working on your app!');
+
+  function changeText() {
+    setText('The Text Changed!');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{outputText}</Text>
+      <Button title="Change Text" onPress={changeText} />
       <StatusBar style="auto" />
     </View>
   );
